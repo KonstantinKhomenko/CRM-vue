d81f929802809ea2377c9cbcc2684f203568af4c
Konstantin Khomenko <kostya5599@gmail.com>
2020-05-06 19:40:15 +0300

    added firebase

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import "materialize-css/dist/js/materialize.min";
 import dateFilter from "./filters/dateFilter";
 import messagePlugin from "./utils/messagePlugin";
 
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 Vue.config.productionTip = false;
 
 Vue.use(messagePlugin);
@@ -15,8 +19,25 @@ Vue.use(Vuelidate);
 
 Vue.filter("dateFilter", dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+firebase.initializeApp({
+  apiKey: "AIzaSyD0z8d_ZutzcaDeeBO2ogIFGeGSgiHgGqY",
+  authDomain: "project-crm-vue.firebaseapp.com",
+  databaseURL: "https://project-crm-vue.firebaseio.com",
+  projectId: "project-crm-vue",
+  storageBucket: "project-crm-vue.appspot.com",
+  messagingSenderId: "605528666247",
+  appId: "1:605528666247:web:3c051c67ad4f8e887a8822",
+  measurementId: "G-DB85N94DFT",
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -82,7 +82,7 @@ export default {
     password: { required, minLength: minLength(6) },
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -92,8 +92,13 @@ export default {
         email: this.email,
         password: this.password,
       };
-      console.log(formData);
-      this.$router.push("/");
+
+      try {
+        await this.$store.dispatch("login", formData);
+        this.$router.push("/");
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 
