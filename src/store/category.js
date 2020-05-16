6442348f66021ccbfa6313a5a7771346d6ec3194
Konstantin Khomenko <kostya5599@gmail.com>
2020-05-16 14:09:26 +0300

    adding categories

---
+++ src/store/category.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+
+export default {
+  actions: {
+    async createCategory({ commit, dispatch }, { title, limit }) {
+      try {
+        const uid = await dispatch("getUid");
+        const category = await firebase
+          .database()
+          .ref(`/users/${uid}/categories`)
+          .push({
+            title,
+            limit,
+          });
+
+        return {
+          title,
+          limit,
+          id: category.key,
+        };
+      } catch (error) {
+        commit("SET_ERROR", error);
+        throw error;
+      }
+    },
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./auth";
 import info from "./info";
+import category from "./category";
 
 Vue.use(Vuex);
 
@@ -21,7 +22,9 @@ export default new Vuex.Store({
   actions: {
     async fetchCurrency() {
       const key = process.env.VUE_APP_FIXER;
-      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,UAH,RUB`);
+      const res = await fetch(
+        `http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,UAH,RUB`
+      );
       return await res.json();
     },
   },
@@ -31,5 +34,6 @@ export default new Vuex.Store({
   modules: {
     auth,
     info,
+    category,
   },
 });
