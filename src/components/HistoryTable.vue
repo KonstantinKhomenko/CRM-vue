563d5d730fbfe605bdafb4e6c5e8ae52bca5860e
Konstantin Khomenko <kostya5599@gmail.com>
2020-05-27 15:51:16 +0300

    record table

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,50 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Сумма</th>
+        <th>Дата</th>
+        <th>Категория</th>
+        <th>Тип</th>
+        <th>Открыть</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(record, i) of records" :key="record.id">
+        <td>{{ i + 1 }}</td>
+        <td>{{ record.amount | currencyFilter("UAH") }}</td>
+        <td>{{ record.date | dateFilter("datetime") }}</td>
+        <td>{{ record.categoryName }}</td>
+        <td>
+          <span class="white-text badge" :class="[record.typeClass]">{{
+            record.typeText
+          }}</span>
+        </td>
+        <td>
+          <button
+            v-tooltip="'Посмотреть детали'"
+            class="btn-small btn"
+            @click="$router.push('/detail/' + record.id)"
+          >
+            <i class="material-icons">open_in_new</i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "HistoryTable",
+
+  props: {
+    records: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
